7d37bab2559a667bbb7e8803fa69b5209c939e08
Esti Shay and Stephanie Mayer <stephanie@>
2017-08-03 10:24:19 -0700

    add test of game start

--- src/test/java/models/AdventureTest.java
+++ src/test/java/models/AdventureTest.java
@@ -1,10 +1,11 @@
 package models;
-
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-/**
- * Created by Guest on 8/3/17.
- */
 public class AdventureTest {
-
+    @Test
+    public void startGame_getResponse_play() throws Exception {
+        Adventure testAdventure = new Adventure ();
+        assertEquals("a", testAdventure.Adventure());
+    }
 }
