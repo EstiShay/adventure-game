ad8e7ebc36a7f8d6deaa9d92bf1d41e695cbf43a
Esti Shay and Stephanie Mayer <stephanie@>
2017-08-03 10:53:23 -0700

    add game start method

--- src/main/java/models/Adventure.java
+++ src/main/java/models/Adventure.java
@@ -11,8 +11,8 @@ public class Adventure {
 
     }
 
-//     public String startGame() {
-//         System.out.println("Setting: August 1944 in partially occupied France.  You are Bessie Stringfield, the Motorcycle Queen of Miami, an African-American motorcycling pioneer. Your skills performing difficult stunts at carnivals and winning races has made you a legend. You are now a motorcycle dispatch rider with the U.S. Army. Your mission, should you choose to accept it, is to carry an encrypted message to the front lines.  Between you and your destination is war-torn country with damaged roads, desperate citizens, and partisan fighters.  And, unfortunately, the racism you have faced your whole life. Do you a) accept the mission, or b) reject it?");
-//         return null;
-//     }
+     public String startGame() {
+         System.out.println("Setting: August 1944 in partially occupied France.  You are Bessie Stringfield, the Motorcycle Queen of Miami, an African-American motorcycling pioneer. Your skills performing difficult stunts at carnivals and winning races has made you a legend. You are now a motorcycle dispatch rider with the U.S. Army. Your mission, should you choose to accept it, is to carry an encrypted message to the front lines.  Between you and your destination is war-torn country with damaged roads, desperate citizens, and partisan fighters.  And, unfortunately, the racism you have faced your whole life. Do you a) accept the mission, or b) reject it?");
+         return null;
+     }
 }
